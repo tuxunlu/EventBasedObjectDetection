f0e50b7ff917e1f042eb371ebd1c33eb17b3bf39
Tuxun Lu <tuxunlu@umd.edu>
2026-06-08 22:04:14 -0400

    Add PYTHONPATH export in activate.sh for improved module accessibility

--- activate.sh
+++ activate.sh
@@ -3,3 +3,4 @@
 #     source activate.sh
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source "${SCRIPT_DIR}/EventBasedObjectDetection/bin/activate"
+export PYTHONPATH="${SCRIPT_DIR}${PYTHONPATH:+:$PYTHONPATH}"
